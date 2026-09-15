ce05e2550c9e69a9b7ad088e9863222cf3f6842d
Carlos <matiasdezela@gmail.com>
2026-09-15 09:00:11 -0500

    Input de los datos implementado

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+def leer_datos(precio, cantidad):
+    # TODO: Implementar funcionalidad
+    pass
+
+print('Bienvenido!')
+precio = input('Ingrese precio: ')
+cantidad = input('Ingrese cantidad: ')
+
+leer_datos(precio, cantidad)
