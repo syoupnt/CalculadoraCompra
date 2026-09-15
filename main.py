35d828b4aa11b0c43731c68c8325e9308acc85a1
Carlos <matiasdezela@gmail.com>
2026-09-15 09:19:03 -0500

    Calcular subtotal implementado

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def leer_datos(datos: list):
     assert len(datos) == 2, 'Cantidad incorrecta de datos'
-    datos[0] = input('Ingrese precio: ')
-    datos[1] = input('Ingrese cantidad: ')
+    datos[0] = float(input('Ingrese precio: '))
+    datos[1] = float(input('Ingrese cantidad: '))
+
+def calcular_subtotal(precio, cantidad):
+    return precio * cantidad
 
 if __name__ == '__main__':
-    print('Bienvenido!')
+    print('Bienvenido!\n')
 
     datos = [
         0, # Precio
@@ -16,6 +19,11 @@ if __name__ == '__main__':
     precio = datos[0]
     cantidad = datos[1]
 
-    print('Comprobando:')
+    print('\nComprobando:')
     print(f'- precio: {precio}')
     print(f'- cantidad: {cantidad}')
+
+    subtotal = calcular_subtotal(precio, cantidad)
+
+    print('\nResultados:')
+    print(f'+ subtotal: {subtotal}\n')
