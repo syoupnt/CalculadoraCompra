62c699ddc02fa86d9c0008c5d90e2c3e327bc75f
Carlos <matiasdezela@gmail.com>
2026-09-15 09:25:28 -0500

    Descuento añadido

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def leer_datos(datos: list):
     assert len(datos) == 2, 'Cantidad incorrecta de datos'
-    datos[0] = float(input('Ingrese precio: '))
-    datos[1] = float(input('Ingrese cantidad: '))
+    datos[0] = float(input('Ingrese precio (S/): '))
+    datos[1] = float(input('Ingrese cantidad:    '))
 
 def calcular_subtotal(precio, cantidad):
     return precio * cantidad
+
+def calcular_descuento(subtotal):
+    return .1 * subtotal
 
 if __name__ == '__main__':
     print('Bienvenido!\n')
@@ -20,10 +23,16 @@ if __name__ == '__main__':
     cantidad = datos[1]
 
     print('\nComprobando:')
-    print(f'- precio: {precio}')
+    print(f'- precio:   S/{precio}')
     print(f'- cantidad: {cantidad}')
 
     subtotal = calcular_subtotal(precio, cantidad)
+    descuento = calcular_descuento(subtotal)
+    total = subtotal - descuento
 
     print('\nResultados:')
-    print(f'+ subtotal: {subtotal}\n')
+    print(s1:=f'+ subtotal:  +S/{subtotal}')
+    print(s2:=f'- descuento: -S/{descuento}')
+    print('-' * max(len(s1), len(s2)))
+    print(f'* Total:        {total}')
+    print()
